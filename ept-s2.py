6f2e2621026803bfad3b69b23a0155b2820f35e7
x42bn6 <x42bn6@gmail.com>
2024-04-29 16:01:46 +0100

    Update ept-s2.py

--- ept-s2.py
+++ ept-s2.py
@@ -47,7 +47,7 @@ class SolvedModel:
             if points == 0:
                 return ""
             return round(points)
-        
+
         def format_points_in_between(points):
             if points == None:
                 return ""
@@ -57,7 +57,7 @@ class SolvedModel:
 
         for t in range(teams):
             teamname = list(currentpoints.keys())[t]
-            
+
             mat[t] = [0] * 11
             mat[t][0] = teamname
             mat[t][1] = format_points_tournament(self.points_s21.get(teamname))
@@ -74,19 +74,19 @@ class SolvedModel:
 
         def get_team_name(t):
             return teamlist[t]
-        
+
         def is_placeholder_team(teamname):
             return teamname in ["NA team", "SA team", "WEU team 1", "WEU team 2", "EEU team", "MENA team", "China team", "SEA team"]
-        
+
         def get_placementbg(tournamentpoints, points):
             if points not in tournamentpoints:
                 return ""
-                
+
             i = tournamentpoints.index(points)
             if i < 4:
                 return f"{{{{PlacementBg/{i+1}}}}} "
             return ""
-        
+
         print("Printing Liquipedia table")
         print()
         print("==What does the threshold scenario look like?==")
@@ -112,17 +112,17 @@ class SolvedModel:
                 print("|-")
                 print('| colspan="99" | Top 8 cutoff')
             print("|-")
-            
+
             teamcomponent = f"{{{{Team|{row[0]}}}}}" if not is_placeholder_team(row[0]) else f"{row[0]} with 0 EPT points"
             s21component = f"{get_placementbg(self.r_s21, row[1])}{row[1]}"
             klcomponent = f"{get_placementbg(self.r_kl, row[3])}{row[3]}"
             s22component = f"{get_placementbg(self.r_s22, row[5])}{row[5]}"
             birminghamcomponent = f"{get_placementbg(self.r_birmingham, row[7])}{row[7]}"
             s23component = f"{get_placementbg(self.r_s23, row[9])}{row[9]}"
-            
+
             print(f'| {(i+1)}')
             print(f'!style="text-align: left;"| {teamcomponent}')
-            
+
             if i == 8:
                 print(f'| style="font-weight: bold; background-color: var(--achievement-placement-down, #cd5b5b);" | {row[10]}')
             else:
@@ -170,7 +170,7 @@ class Model:
             'PSG Quest': 100,
             'Talon Esports': 100
         }
-    
+
         self.points_s21_kl = {
             'OG': -384,
             'Shopify Rebellion': -2000,
@@ -179,7 +179,7 @@ class Model:
             'Entity': -264,
             'Talon Esports': -100
         }
-    
+
         self.points_kl = {
             'BetBoom Team': 2400,
             'Team Falcons': 1680,
@@ -192,14 +192,14 @@ class Model:
             'LGD Gaming': 420,
             'Azure Ray': 4800,
         }
-    
+
         self.points_kl_s22 = {
             'Xtreme Gaming': 3360,
             'Team Secret': -234,
             'Tundra Esports': -780,
             'Azure Ray': -4800
         }
-    
+
         self.points_s22 = {
             'BetBoom Team': 3500,
             'Xtreme Gaming': 2800,
@@ -218,7 +218,7 @@ class Model:
             '1win': 42,
             'Azure Ray': 175
         }
-    
+
         # Placeholder 0 EPT point teams in qualifier
         self.qualifier_teams = {
             #'NA team': 0,
@@ -243,14 +243,14 @@ class Model:
         self.qualifier_teams.pop('Team Zero')
         self.qualifier_teams.pop('Nigma Galaxy')
         self.qualifier_teams.pop('Estar_Backs')
-    
+
         self.points_s22_birmingham = {}
-    
+
         self.points_birmingham_s23 = {}
-    
+
         # Any changes after S23
         self.points_s23_riyadh = {}
-    
+
         self.currentpoints = Counter_tweaked(self.points_s21) + Counter_tweaked(self.points_s21_kl) + \
             Counter_tweaked(self.points_kl) + Counter_tweaked(self.points_kl_s22) + \
             Counter_tweaked(self.points_s22) + Counter_tweaked(self.points_s22_birmingham) + \
@@ -258,7 +258,7 @@ class Model:
             Counter_tweaked(self.qualifier_teams)
         self.teams = len(self.currentpoints)
         self.teamlist = list(self.currentpoints.keys())
-    
+
         self.na_qualifier = ['Shopify Rebellion', 'nouns']
         self.na_qualifier.remove('nouns')
         self.na_qualifier.remove('Shopify Rebellion')
@@ -289,14 +289,14 @@ class Model:
         self.sea_qualifier.remove('Geek Fam')
         self.sea_qualifier.remove('Talon Esports')
         self.sea_qualifier.remove('Aurora')
-    
+
         self.r_s21        = (2400, 2000, 1600, 1280, 880,  880,  400,  400,  200, 200, 100, 100)
         self.r_kl         = (4800, 3600, 3000, 2400, 1680, 1680, 780,  780,  420, 420, 210, 210)
         self.r_s22        = (4200, 3500, 2800, 2240, 1680, 1680, 1400, 1400, 840, 560, 350, 350, 175, 175, 98, 98, 42, 42)
         self.r_birmingham = (6400, 4800, 4000, 3200, 2240, 2240, 1040, 1040, 560, 560, 280, 280)
         self.r_s23        = (6000, 5000, 4000, 3200, 2200, 2200, 1000, 1000, 500, 500, 250, 250)
         self.placements = 12
-    
+
         self.birmingham_teams = ['BetBoom Team', 'Xtreme Gaming', 'Team Falcons', 'Gaimin Gladiators', \
                                  'Team Spirit', 'Team Liquid', 'G2.iG', 'Shopify Rebellion', 'Tundra Esports', 'HEROIC', '1win', 'Talon Esports']
         # 1win visa issues
@@ -307,25 +307,25 @@ class Model:
 
         self.birmingham_highest = [-1 for i in range(self.teams)]
         self.s23_highest = [-1 for i in range(self.teams)]
-        
+
         self.model = cp_model.CpModel()
 
     def team_index(self, t):
         return list(self.currentpoints.keys()).index(t)
-    
+
     def build(self):
         model = self.model
         teams = self.teams
         teamlist = self.teamlist
         placements = self.placements
         currentpoints = self.currentpoints
-        
+
         x_birmingham = [[model.NewBoolVar(f'x_birmingham_{i}_{j}') for j in range(placements)] for i in range(teams)]
         x_s23        = [[model.NewBoolVar(f'x_s23_{i}_{j}') for j in range(placements)] for i in range(teams)]
         d_birmingham = [model.NewIntVar(0, 99999, f'd_birmingham_{i}') for i in range(teams)]
         d_s23        = [model.NewIntVar(0, 99999, f'd_s23_{i}') for i in range(teams)]
         d            = [model.NewIntVar(0, 99999, f'd_{i}') for i in range(teams)]
-        
+
         def team_can_finish_between(tournament, tournament_enum, team, best, worst):
             t = teamlist.index(team)
             sum = 0
@@ -348,7 +348,7 @@ class Model:
                 t = teamlist.index(group_team)
                 sum += tournament[t][9 - 1 + to_add]
             model.Add(sum == 1)
-            
+
             # One of these finishes last
             sum = 0
             for group_team in group:
@@ -371,24 +371,24 @@ class Model:
                 t = teamlist.index(group_team)
                 sum += tournament[t][5 - 1 + to_add]
             model.Add(sum == 1)
-        
+
         #############################
         # ESL One Birmingham
         #############################
         add_group_constraint(x_birmingham, ['BetBoom Team', 'Team Liquid', 'G2.iG', 'Talon Esports', 'Team Falcons', 'Shopify Rebellion'], 0)
         add_group_constraint(x_birmingham, ['HEROIC', 'OG', 'Tundra Esports', 'Team Spirit', 'Gaimin Gladiators', 'Xtreme Gaming'], 1)
-        
-        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'BetBoom Team', 1, 6)
-        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'Xtreme Gaming', 1, 8)
-        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'Team Falcons', 1, 6)
+
+        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'BetBoom Team', 2, 2)
+        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'Xtreme Gaming', 7, 8)
+        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'Team Falcons', 1, 1)
         team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'Gaimin Gladiators', 11, 12)
         team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'Team Spirit', 9, 10)
-        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'Team Liquid', 1, 8)
-        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'OG', 1, 6)
-        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'G2.iG', 1, 8)
+        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'Team Liquid', 5, 6)
+        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'OG', 4, 4)
+        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'G2.iG', 5, 6)
         team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'Shopify Rebellion', 11, 12)
-        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'Tundra Esports', 1, 6)
-        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'HEROIC', 1, 8)
+        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'Tundra Esports', 3, 3)
+        team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'HEROIC', 7, 8)
         team_can_finish_between(x_birmingham, Tournament.BIRMINGHAM, 'Talon Esports', 9, 10)
 
         seventh_eighth(x_birmingham, ['Team Liquid', 'HEROIC'], 0)
@@ -411,17 +411,17 @@ class Model:
         team_can_finish_between(x_s23, Tournament.S23, 'Azure Ray', 1, 12)
         team_can_finish_between(x_s23, Tournament.S23, 'PSG Quest', 1, 12)
         team_can_finish_between(x_s23, Tournament.S23, 'Natus Vincere', 1, 12)
-        
+
         # ESL One Birmingham constraints
         # Qualified teams
         for t in self.birmingham_teams:
             model.Add(sum(x_birmingham[self.team_index(t)]) == 1)
-    
+
         # DreamLeague Season 23 constraints
         # Already-qualified teams
         for t in self.s23_teams:
             model.Add(sum(x_s23[self.team_index(t)]) == 1)
-    
+
         # Regional constraints
         # Basically, within each region, only one team can qualify and thus place
         # So if there are two qualified teams for a region, there are two rows, but only one 1 in both rows
@@ -440,12 +440,12 @@ class Model:
         #add_regional_constraint(self.mena_qualifier, 1, x_s23, model)
         #add_regional_constraint(self.china_qualifier, 1, x_s23, model)
         #add_regional_constraint(self.sea_qualifier, 1, x_s23, model)
-    
+
         # One placement per team
         for p in range(placements):
             model.Add(sum(x_birmingham[i][p] for i in range(teams)) == 1)
             model.Add(sum(x_s23[i][p] for i in range(teams)) == 1)
-        
+
         # Points
         for t in self.currentpoints:
             teamindex = self.team_index(t)
@@ -465,22 +465,22 @@ class Model:
                     model.Add(d[i] - d[j] <= (1 - aux[(i, j)]) * M)
                     model.Add(d[j] - d[i] <= aux[(i, j)] * M)
             ranks[i] = sum(aux[(i, j)] for j in range(teams))
-        
+
         return [model, x_birmingham, x_s23, d_birmingham, d_s23, d, aux, ranks]
-    
+
     def optimise(self, team_to_optimise, show_all, maxobjectivevalue):
         teams = self.teams
         teamlist = self.teamlist
         currentpoints = self.currentpoints
-        
+
         [model, x_birmingham, x_s23, d_birmingham, d_s23, d, aux, ranks] = self.build()
-        
+
         model.Add(ranks[team_to_optimise] > 8)
         model.Maximize(d[team_to_optimise])
 
         def flatten_array(a):
             return reduce(lambda z, y :z + y, a)
-        
+
         # If this team can't breach the best maximum so far, don't bother
         if not show_all:
             teamname = teamlist[team_to_optimise]
@@ -498,12 +498,12 @@ class Model:
                     maxpointsobtainable += self.r_s23[0]
             elif teamname in self.na_qualifier or teamname in self.sa_qualifier or teamname in self.weu_qualifier or teamname in self.eeu_qualifier or teamname in self.mena_qualifier or teamname in self.china_qualifier or teamname in self.sea_qualifier:
                 maxpointsobtainable += self.r_s23[0]
-    
+
             maxpointsobtainable += self.currentpoints[self.teamlist[team_to_optimise]]
             if maxpointsobtainable < maxobjectivevalue:
                 print(f"Skipping {teamlist[team_to_optimise]} as {maxpointsobtainable} < {maxobjectivevalue}")
                 return self.tounsolvedmodel(team_to_optimise)
-        
+
         solver = cp_model.CpSolver()
         status = solver.Solve(model)
         if status == cp_model.OPTIMAL:
@@ -543,7 +543,7 @@ def main():
         print()
 
     max_model.printsolution()
-    
+
     print("Done")
 
 if __name__ == "__main__":
